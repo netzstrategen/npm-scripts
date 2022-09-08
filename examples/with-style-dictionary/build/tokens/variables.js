0eeb63e6a4dde16e3f836fa7cfcafa818a4a76d3
Italo Devoto Ramella <italodr@gmail.com>
2022-09-08 12:28:26 +0200

    Fix style dictionary (#5)

---
+++ examples/with-style-dictionary/build/tokens/variables.js
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 08 Sep 2022 10:24:58 GMT
+ */
+
+export const ColorsAccentInteractiveDefault = "#1890FF";
+export const ColorsAccentInteractiveHover = "#40A9FF";
+export const ColorsAccentInteractiveActive = "#096DD9";
+export const ColorsAccentBrand = "#183b2c";
+export const ColorsFg300 = "#BFBFBF";
+export const ColorsFg400 = "#878787";
+export const ColorsBg100 = "#FFFFFF";
+export const ColorsBg200 = "#FAFAFA";
+export const ColorsBg300 = "#F5F5F5";
+export const ColorsBg400 = "#F0F0F0";
+export const ColorsBg500 = "#D9D9D9";
+export const ColorsBg600 = "#BFBFBF";
+export const ColorsBg700 = "#8C8C8C";
+export const ColorsBg800 = "#595959";
+export const ColorsBg900 = "#000000";
+export const FontNonserif = "Roboto";
+export const H1MediumMaxFontFamily = "Roboto";
+export const H1MediumMaxFontWeight = "Medium";
+export const H1MediumMaxLineHeight = "120%";
+export const H1MediumMaxFontSize = "2.7587890624999996rem";
